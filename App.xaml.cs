e1fc8071b786881f50453e3708f8022e208b8c65
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2024-02-25 12:09:57 +0100

    Update 1 - Added Segoe Fluent Icons font - Added Serilog for debugging - Did some design on the main window

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using System;
+using System.Runtime.InteropServices;
 using System.Windows;
+
+// Imported
+using Serilog;
 
 namespace Xenia_Manager
 {
@@ -9,6 +12,41 @@ namespace Xenia_Manager
     /// </summary>
     public partial class App : Application
     {
-    }
+        // This is needed for Console to show up when using argument -console
+        [DllImport("Kernel32")]
+        public static extern void AllocConsole();
+        [DllImport("Kernel32")]
+        public static extern void FreeConsole();
+        private bool logging = false;
 
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Serilog.Log.Logger = Log.Logger; // Initializing Logger
+                                             // Checks for all of the Launch Arguments
+            if (e.Args.Contains("-console"))
+            {
+                AllocConsole();
+                logging = true;
+            }
+            // Creating Logger Configuration
+            if (logging)
+            {
+                Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console(
+                    outputTemplate: "{Timestamp:dd-MM-yyyy HH:mm:ss}|{Level}|{Message}{NewLine}{Exception}")
+                //.WriteTo.File("Logs.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+            }
+            else
+            {
+                Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console(
+                    outputTemplate: "{Timestamp:dd-MM-yyyy HH:mm:ss}|{Level}|{Message}{NewLine}{Exception}")
+                //.WriteTo.File("Logs.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+            }
+
+            Log.Information("App Loaded");
+        }
+    }
 }
---
+++ Windows/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+// Imported
+using Serilog;
+
+namespace Xenia_Manager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+    }
+}
